0f7dd786290fe0dcca66ddcbdc09fc28ce122e67
Jhinko <baineynathan@gmail.com>
2022-02-10 14:08:08 +0100

    Add all files for nav bar menu

---
+++ vues/NavBar.php
@@ -0,0 +1,32 @@
+<header>
+        <div id="navbar">
+            <div id="left">
+                <div class="leftIcon"><img src="Img/Roi_burgonde-livreI.jpg" alt="logo192" width="100" height="100"
+                        id="Burgonde"></div>
+                <div class="leftIcon"><a href="HomePage.html"><img
+                            src="https://img.icons8.com/ios-filled/50/ffffff/home.png" /></a></div>
+                <div class="leftIcon"><a href="#"><img
+                            src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png" /></a></div>
+                <div class="leftIcon" id="specIcon"><button id="toggle"><img
+                            src="https://img.icons8.com/external-prettycons-solid-prettycons/50/ffffff/external-notification-essentials-prettycons-solid-prettycons.png" /></button>
+                    <div id="menu">
+                        <ul>
+                            <li><a href="#home">Message ici</a></li>
+                            <li><a href="#about">Message ici</a></li>
+                            <li><a href="#contact">Message ici</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <img src="Img/logoBlancComplet.png" alt="logoBlancComplet" width="289px" height="93px" />
+
+            <ul>
+                <input type="search" name="SearchBar" id="SearchBar" placeholder="+ Ajoutez en cas clinique">
+            </ul>
+        </div>
+    </header>
+    
+
+
