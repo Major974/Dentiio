1aac45a90829886312bb31777c0fa40990e338d5
major55 <melicourtjul@outlook.fr>
2022-02-10 14:19:15 +0100

    major out

---
+++ vues/fav.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Vos Favoris</h1>
+
+
+    <div class="card" style="width: 18rem;">
+        <img src="../img/D2QNWjMXQAA4jdb.jpg" class="card-img-top" alt="dev-meme-1">
+        <div class="card-body">
+            <title>Greffe de gencive</title>
+            <p class="card-text">L’améloblastome est une tumeur odontogène
+                bénigne, d’agressivité locale, avec un taux de
+                transformation maligne très faible. Elle est
+                néanmoins connue très récidivante après
+                traitement. Il s’agit de la tumeur odontogène
+                bénigne la plus fréquente dans les pays ...</p>
+            <a href="#" class="card-link">@</a>
+            <a href="#" class="card-link">@</a>
+            <a href="#" class="card-link">@</a>
+            <p>nom</p>
+            <p>role</p>
+        </div>
+    </div>
+
+
+    <div class="card" style="width: 18rem;">
+        <img src="../img/index.jpeg" class="card-img-top" alt="dev-meme-2">
+        <div class="card-body">
+            <title>Parodontite aîgue</title>
+            <p class="card-text">L’énucléation et l’exérèse de la lésion avaient été
+                réalisées sous anesthésie locorégionale. En
+                macroscopie, la pièce opératoire était ferme avec
+                des contours irréguliers (Fig 5). L’examen
+                anatomopathologique de la lésion révélait la
+                présence d’une prolifération tumorale ...</p>
+            <a href="#" class="card-link">@</a>
+            <a href="#" class="card-link">@</a>
+            <a href="#" class="card-link">@</a>
+            <p>nom</p>
+            <p>role</p>
+
+        </div>
+    </div>
+
+</body>
+<footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</footer>
+
+</html>
