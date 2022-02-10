b9c360a6ef74503a17deec38bd102148e3e54536
forbidden Betta <forbiddenBetta@gmail.com>
2022-02-10 16:04:12 +0100

    Mid afteroon

--- overlay_categorie.php
+++ overlay_categorie.php
@@ -8,22 +8,32 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="assets/style.css">
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="Styles/NavStyle.css">
 
 </head>
+<?php
+include("vues/NavBar.php");
+?>
 
 <body>
-    <div class="main_container_categorie">
+    <div>
+
         <div class="d-flex flex-row">
 
-            <div class="opacity_square">
-
+            <div class="d-flex flex-column  p-2 " style="height:90vh; width:20vw">
+                <div class="category_button p-2">Pathologie ></div>
+                <div class="category_button p-2">Traitement ></div>
+                <div class="category_button p-2">Spécialité ></div>
+                <div class="category_button p-2">Anatomie ></div>
             </div>
-            <div class="cat_container"></div>
+            <div class="p-2 bd-highlight bg-warning bg-gradient flex-grow-1" style="width:50vw;">
+                Opacity here
+            </div>
         </div>
     </div>
+    <!-- </div> -->
 </body>
 
 </html>
