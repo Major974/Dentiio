33b525b5978b7a6b29a4ffdec728490f2bbfebc5
forbidden Betta <forbiddenBetta@gmail.com>
2022-02-11 12:22:58 +0100

    LEts gooo

--- vues/Overlay_categorie.php
+++ vues/Overlay_categorie.php
@@ -7,29 +7,53 @@
 include("../vues/NavBar.php");
 ?>
 
-<body>
-    <div>
+<div id="myNav" class="overlay">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <div class="overlay-content">
 
-        <div class="main_container_categorie d-flex flex-row">
 
-            <div class="cat_container d-flex flex-column  p-2">
-                <div class="category_button p-2">Pathologie
-                    <img src="img/outline_arrow_forward_ios_black_24dp.png" style="float:right" alt="open_category">
+        <div>
+
+            <div class="main_container_categorie d-flex flex-column">
+                <div></div>
+
+                <div class="cat_container d-flex flex-column  p-2">
+                    <div class="category_button p-2">
+                        <a href="Inscription.php">Pathologie
+                            <img src="/img/outline_arrow_forward_ios_black_24dp.png" style="float:right">
+                        </a>
+                    </div>
+                    <div class="category_button p-2">
+                        <a href="Inscription.php">Traitement
+                            <img src="/img/outline_arrow_forward_ios_black_24dp.png" style="float:right" style="float:right" alt="">
+                        </a>
+                    </div>
+                    <div class="category_button p-2">
+                        <a href="Inscription.php">Spécialité
+                            <img src="/img/outline_arrow_forward_ios_black_24dp.png" style="float:right" alt="">
+                        </a>
+                    </div>
+                    <div class="category_button p-2">
+                        <a href="Inscription.php">Anatomie
+                            <img src="/img/outline_arrow_forward_ios_black_24dp.png" style="float:right" alt="">
+                        </a>
+                    </div>
                 </div>
-                <div class="category_button p-2">Traitement
-                    <img src="img/outline_arrow_forward_ios_black_24dp.png" style="float:right" style="float:right"alt="">
-                </div>
-                <div class="category_button p-2">Spécialité
-                    <img src="img/outline_arrow_forward_ios_black_24dp.png" style="float:right" alt="">
-                </div>
-                <div class="category_button p-2">Anatomie
-                    <img src="img/outline_arrow_forward_ios_black_24dp.png" style="float:right" alt="">
-                </div>
-            </div>
-            <div class="opacity_container bd-highlight flex-grow-1">
+
             </div>
         </div>
-    </div>
-</body>
 
-</html>
+
+    </div>
+</div>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+<script>
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+</script>
