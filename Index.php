42a46260b2ea67ee1fda32e2498fcfc37cd0b9f4
forbidden Betta <forbiddenBetta@gmail.com>
2022-02-11 11:28:10 +0100

    Bouge des fichiers dans /vues

--- Index.php
+++ Index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include 'Include/Head.php'; ?>
+<?php include '../Include/Head.php'; ?>
 
 <body>
     <?php
-    include("vues/NavBar.php");
+    include("../vues/NavBar.php");
     ?>
-    <a href="Inscription.php">Inscrip</a><br>
-    <a href="Connexion.php">Connexion</a><br>
-    <a href="Overlay_categorie.php">Overlay</a><br>
+    <a href="vues/Inscription.php">Inscrip</a><br>
+    <a href="vues/Connexion.php">Connexion</a><br>
+    <a href="vues/Overlay_categorie.php">Overlay</a><br>
     <a href="Fav.php">Fav</a><br>
     <a href=""></a><br>
 
