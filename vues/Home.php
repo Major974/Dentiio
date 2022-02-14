fd9a37206f1c478993e328abadb7acbeb6f5899c
forbidden Betta <forbiddenBetta@gmail.com>
2022-02-14 16:45:47 +0100

    Le merge de la dernière minute

--- vues/Home.php
+++ vues/Home.php
@@ -123,26 +123,17 @@ include("../vues/NavBar.php");
                                 néanmoins connue très récidivante après
                                 traitement. Il s’agit de la tumeur odontogène
                                 bénigne la plus fréquente dans les pays ...</p>
-                            <div class="card-body">
-                                <title>Greffe de gencive</title>
-                                <p class="card-text">L’améloblastome est une tumeur odontogène
-                                    bénigne, d’agressivité locale, avec un taux de
-                                    transformation maligne très faible. Elle est
-                                    néanmoins connue très récidivante après
-                                    traitement. Il s’agit de la tumeur odontogène
-                                    bénigne la plus fréquente dans les pays ...</p>
-                                <img src="" class="" alt="">
-                                <div id="cercle"></div>
-                                <div class="namecl">
-                                    <a>Arthur</a>
-                                    <p>étudiant</p>
-                                </div>
-                                <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                                    <a id="cercle-bot3">4.8</a>
-                                </div>
-                                <div id="cercle-bot2"><img src="../img/logo_chat_bleu.png" class="card-img" alt="">
-                                    <a id="cercle-bot3">18</a>
-                                </div>
+                            <img src="" class="" alt="">
+                            <div id="cercle"></div>
+                            <div class="namecl">
+                                <a>Arthur</a>
+                                <p>étudiant</p>
+                            </div>
+                            <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
+                                <a id="cercle-bot3">4.8</a>
+                            </div>
+                            <div id="cercle-bot2"><img src="../img/logo_chat_bleu.png" class="card-img" alt="">
+                                <a id="cercle-bot3">18</a>
                             </div>
                         </div>
                     </div>
@@ -238,3 +229,8 @@ include("../vues/NavBar.php");
 
     </div>
 </body>
+
+<footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="../Script/Notif.js"></script>
+</footer>
--- vues/Profil.php
+++ vues/Profil.php
@@ -27,7 +27,7 @@
     </div>
 
 
-    
+
     <div style="top:200px; position: absolute;">
         <div class="row">
             <div class="column">
@@ -46,28 +46,28 @@
                             néanmoins connue très récidivante après
                             traitement. Il s’agit de la tumeur odontogène
                             bénigne la plus fréquente dans les pays ...</p>
-                            <img src="" class="" alt="">
-                            <div id="cercle"></div>
-                            <div class="namecl">
-                                <a>Arthur</a>
-                                <p>étudiant</p>
-                            </div>
-                            <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
+                        <img src="" class="" alt="">
+                        <div id="cercle"></div>
+                        <div class="namecl">
+                            <a>Arthur</a>
+                            <p>étudiant</p>
+                        </div>
+                        <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
                             <a id="cercle-bot3">4.8</a>
                         </div>
                         <div id="cercle-bot2"><img src="../img/logo_chat_bleu.png" class="card-img" alt="">
-                        <a id="cercle-bot3">18</a>
+                            <a id="cercle-bot3">18</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="column1">
-            <div class="card" style="width: 20rem;">
-                <div id="cercle-top"><img src="../img/Etoile_Bleu_PLeine.png" class="card-img" alt="dev-meme-1"></div>
-                <img src="../img/Git_branch_meme.webp" class="card-img-top" alt="dev-meme-2">
-                <div class="">
-                    <div class="barbleu3"><a>Machoire</a></div>
-                    <div class="barbleu4"><a>Parodontie</a></div>
+            <div class="column1">
+                <div class="card" style="width: 20rem;">
+                    <div id="cercle-top"><img src="../img/Etoile_Bleu_PLeine.png" class="card-img" alt="dev-meme-1"></div>
+                    <img src="../img/Git_branch_meme.webp" class="card-img-top" alt="dev-meme-2">
+                    <div class="">
+                        <div class="barbleu3"><a>Machoire</a></div>
+                        <div class="barbleu4"><a>Parodontie</a></div>
                         <div class="barbleu5"><a>Extraction</a></div>
                     </div>
                     <div class="card-body">
@@ -78,30 +78,30 @@
                             des contours irréguliers (Fig 5). L’examen
                             anatomopathologique de la lésion révélait la
                             présence d’une prolifération tumorale ...</p>
-                            <img src="" class="" alt="">
-                            <div id="cercle"></div>
-                            <div class="namecl">
-                                <a>Nikita</a>
-                                <p>inplantologue</p>
-                            </div>
-                            <div id="cercle-bot1">
-                                <img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                                <a id="cercle-bot3">4.3</a>
-                            </div>
-                            <div id="cercle-bot2">
-                                <img src="../img/logo_chat_bleu.png" class="card-img" alt="">
-                                <a id="cercle-bot3">13</a>
+                        <img src="" class="" alt="">
+                        <div id="cercle"></div>
+                        <div class="namecl">
+                            <a>Nikita</a>
+                            <p>inplantologue</p>
+                        </div>
+                        <div id="cercle-bot1">
+                            <img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
+                            <a id="cercle-bot3">4.3</a>
+                        </div>
+                        <div id="cercle-bot2">
+                            <img src="../img/logo_chat_bleu.png" class="card-img" alt="">
+                            <a id="cercle-bot3">13</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-        
-    </body>
-    <footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <script src="../Script/Notif.js"></script>
-    </footer>
-    
-    </html>
+
+</body>
+<footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="../Script/Notif.js"></script>
+</footer>
+
+</html>
