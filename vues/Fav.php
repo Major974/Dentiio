3289a444e40a4c81c8991ae710b8044667e1b44f
major55 <melicourtjul@outlook.fr>
2022-02-14 16:26:53 +0100

    last retouch

--- vues/Fav.php
+++ vues/Fav.php
@@ -31,30 +31,22 @@ include("Overlay_categorie.php");
                             néanmoins connue très récidivante après
                             traitement. Il s’agit de la tumeur odontogène
                             bénigne la plus fréquente dans les pays ...</p>
-                        <div class="card-body">
-                            <title>Greffe de gencive</title>
-                            <p class="card-text">L’améloblastome est une tumeur odontogène
-                                bénigne, d’agressivité locale, avec un taux de
-                                transformation maligne très faible. Elle est
-                                néanmoins connue très récidivante après
-                                traitement. Il s’agit de la tumeur odontogène
-                                bénigne la plus fréquente dans les pays ...</p>
-                            <img src="" class="" alt="">
-                            <div id="cercle"></div>
-                            <div class="namecl">
-                                <a>Arthur</a>
-                                <p>étudiant</p>
-                            </div>
-                            <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                                <a id="cercle-bot3">4.8</a>
-                            </div>
-                            <div id="cercle-bot2"><img src="../img/logo_chat_bleu.png" class="card-img" alt="">
-                                <a id="cercle-bot3">18</a>
-                            </div>
+                        <img src="" class="" alt="">
+                        <div id="cercle"></div>
+                        <div class="namecl">
+                            <a>Arthur</a>
+                            <p>étudiant</p>
+                        </div>
+                        <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
+                            <a id="cercle-bot3">4.8</a>
+                        </div>
+                        <div id="cercle-bot2"><img src="../img/logo_chat_bleu.png" class="card-img" alt="">
+                            <a id="cercle-bot3">18</a>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div class="column1">
                 <div class="card" style="width: 20rem;">
                     <div id="cercle-top"><img src="../img/Etoile_Bleu_PLeine.png" class="card-img" alt="dev-meme-1"></div>
@@ -72,33 +64,26 @@ include("Overlay_categorie.php");
                             des contours irréguliers (Fig 5). L’examen
                             anatomopathologique de la lésion révélait la
                             présence d’une prolifération tumorale ...</p>
-                        <div class="card-body">
-                            <title>Parodontite aîgue</title>
-                            <p class="card-text">L’énucléation et l’exérèse de la lésion avaient été
-                                réalisées sous anesthésie locorégionale. En
-                                macroscopie, la pièce opératoire était ferme avec
-                                des contours irréguliers (Fig 5). L’examen
-                                anatomopathologique de la lésion révélait la
-                                présence d’une prolifération tumorale ...</p>
-                            <img src="" class="" alt="">
-                            <div id="cercle"></div>
-                            <div class="namecl">
-                                <a>Nikita</a>
-                                <p>inplantologue</p>
-                            </div>
-                            <div id="cercle-bot1">
-                                <img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                                <a id="cercle-bot3">4.3</a>
-                            </div>
-                            <div id="cercle-bot2">
-                                <img src="../img/logo_chat_bleu.png" class="card-img" alt="">
-                                <a id="cercle-bot3">13</a>
-                            </div>
+                        <img src="" class="" alt="">
+                        <div id="cercle"></div>
+                        <div class="namecl">
+                            <a>Nikita</a>
+                            <p>inplantologue</p>
+                        </div>
+                        <div id="cercle-bot1">
+                            <img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
+                            <a id="cercle-bot3">4.3</a>
+                        </div>
+                        <div id="cercle-bot2">
+                            <img src="../img/logo_chat_bleu.png" class="card-img" alt="">
+                            <a id="cercle-bot3">13</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    </div>
     </div>
 </body>
 <footer>
