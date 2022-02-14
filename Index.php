9e4b4bdb27859218880da52cc82f6b9fa14bbab4
forbidden Betta <forbiddenBetta@gmail.com>
2022-02-14 12:32:22 +0100

    Mon progress

--- Index.php
+++ Index.php
@@ -10,6 +10,7 @@
     <a href="vues/Inscription.php">Inscrip</a><br>
     <a href="vues/Connexion.php">Connexion</a><br>
     <a href="vues/Overlay_categorie.php">Overlay</a><br>
+    <a href="vues/Home.php">Home</a><br>
     <a href=""></a><br>
 
 </body>
--- vues/Fav.php
+++ vues/Fav.php
@@ -31,15 +31,6 @@ include("Overlay_categorie.php");
                             néanmoins connue très récidivante après
                             traitement. Il s’agit de la tumeur odontogène
                             bénigne la plus fréquente dans les pays ...</p>
-                        <img src="" class="" alt="">
-                        <div id="cercle"></div>
-                        <div class="namecl">
-                            <a>Arthur</a>
-                            <p>étudiant</p>
-                        </div>
-                        <div id="cercle-bot1"><img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                            <a id="cercle-bot3">4.8</a>
-                        </div>
                         <div class="card-body">
                             <title>Greffe de gencive</title>
                             <p class="card-text">L’améloblastome est une tumeur odontogène
@@ -81,16 +72,6 @@ include("Overlay_categorie.php");
                             des contours irréguliers (Fig 5). L’examen
                             anatomopathologique de la lésion révélait la
                             présence d’une prolifération tumorale ...</p>
-                        <img src="" class="" alt="">
-                        <div id="cercle"></div>
-                        <div class="namecl">
-                            <a>Nikita</a>
-                            <p>inplantologue</p>
-                        </div>
-                        <div id="cercle-bot1">
-                            <img src="../img/dent_bleu_pleine.png" class="card-img" alt="">
-                            <a id="cercle-bot3">4.3</a>
-                        </div>
                         <div class="card-body">
                             <title>Parodontite aîgue</title>
                             <p class="card-text">L’énucléation et l’exérèse de la lésion avaient été
